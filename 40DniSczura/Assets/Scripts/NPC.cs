2ac49f6b6d39db30e45704481269ae5d79deb328
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-03-21 17:46:02 +0100

    Poprawki kamery + dialog
    Działa :DDD

---
+++ 40DniSczura/Assets/Scripts/NPC.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialogbox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogbox.activeInHierarchy)
+            {
+                dialogbox.SetActive(false);
+            }
+            else
+            {
+                dialogbox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogbox.SetActive(false);
+        }
+    }
+}
