f31548958885a3d63cb3bda7a2de8e3cfd32da2c
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-03-21 19:07:04 +0100

    Dokończony system dialogów
    Dialogi mogą mieć więcej niż 1 linijkę
    Dialogi poprawinie wyświetlają portrety i imiona postaci
    Dialogi mogą uruchamiać guesty i triggerować fazy questów

--- 40DniSczura/Assets/Scripts/NPC.cs
+++ 40DniSczura/Assets/Scripts/NPC.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
-    public GameObject dialogbox;
+    public GameObject dialogBox;
     public Text dialogText;
-    public string dialog;
+    public Text nameText;
+    public Image portrait;
+    public string[] dialog;
     public bool playerInRange;
+    public int currentLine;
+
+    public bool beginQuest, triggerQuest;
+    public int questID;
+    public string questTrigger;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +28,23 @@ public class NPC : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
         {
-            if (dialogbox.activeInHierarchy)
+            if (dialogBox.activeInHierarchy && (currentLine == dialog.Length))
             {
-                dialogbox.SetActive(false);
+                ExitDialogue();
             }
             else
             {
-                dialogbox.SetActive(true);
-                dialogText.text = dialog;
+                CameraContorller.instance.lerpSpeed = 0.1f;
+                CameraContorller.instance.anchor = transform;
+                CameraContorller.instance.cameraSize = 4f;
+                dialogBox.SetActive(true);
+                CheckIfName();
+                dialogText.text = dialog[currentLine];
+                currentLine++;
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -43,7 +57,37 @@ public class NPC : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            dialogbox.SetActive(false);
+            ExitDialogue();
+        }
+    }
+
+    private void ExitDialogue()
+    {
+        CameraContorller.instance.anchor = PlayerController.instance.transform;
+        CameraContorller.instance.cameraSize = 5f;
+        CameraContorller.instance.lerpSpeed = 0.2f;
+        currentLine = 0;
+
+        if(beginQuest)
+        {
+            QuestManager.instance.questList[questID].questStarted = true;
+        }
+        if(triggerQuest)
+        {
+            QuestManager.instance.TriggerQuest(questID, questTrigger);
+        }
+
+        dialogBox.SetActive(false);
+    }
+
+    private void CheckIfName()
+    {
+        if(dialog[currentLine].StartsWith("--"))
+        {
+            nameText.text = dialog[currentLine].Replace("--", "") + ":";
+            portrait.sprite = GameManager.instance.GetPortrait(dialog[currentLine].Replace("--", ""));
+            Debug.Log(dialog[currentLine].Replace("--", ""));
+            currentLine++;
         }
     }
 }
