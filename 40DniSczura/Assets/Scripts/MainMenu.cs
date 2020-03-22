e3dcbdb14a5fd4271640cf88db0f77205084f554
Paweł Bałdowski <pbaldowski1@wp.pl>
2020-03-23 00:37:44 +0100

    Menusy

--- 40DniSczura/Assets/Scripts/MainMenu.cs
+++ 40DniSczura/Assets/Scripts/MainMenu.cs
@@ -16,4 +16,10 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Quit!");
     }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
