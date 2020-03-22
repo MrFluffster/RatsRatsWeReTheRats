03d806033ca5231783b70927d0dd70a095e17918
Ortuy <60518287+Ortuy@users.noreply.github.com>
2020-03-22 18:44:55 +0100

    AI działa i guess. pomocy.
    Patrole - działają
    Pościgi - działają
    Patrole w połączeniu z pościgami - działają
    Biedny mózg programisty - nie działa

---
+++ 40DniSczura/Assets/Scripts/PatrolPoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        AIController patrolling = collision.GetComponent<AIController>();
+        if(patrolling != null)
+        {
+            patrolling.atPatrolPoint = true;
+            if (patrolling.currentGoal >= patrolling.patrolRoute.Length - 1)
+            {
+                patrolling.currentGoal = 0;
+            }
+            else
+            {
+                patrolling.currentGoal++;
+            }
+            Debug.Log(patrolling.currentGoal);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        AIController patrolling = collision.GetComponent<AIController>();
+        if (patrolling != null)
+        {
+            patrolling.atPatrolPoint = false;
+        }
+    }
+}
