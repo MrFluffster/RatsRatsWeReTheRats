cfa55418e98e37c6259f14fdfa71cd68b3ae6c06
Paweł Bałdowski <pbaldowski1@wp.pl>
2020-03-25 23:00:18 +0100

    Yay

--- 40DniSczura/Assets/Scripts/UIHolder.cs
+++ 40DniSczura/Assets/Scripts/UIHolder.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHolder : MonoBehaviour
 {
     public static UIHolder instance;
+    public bool fade;
+    public bool unfade;
+    public float fadeSpeed;
+
+    public Image fadeScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +24,40 @@ public class UIHolder : MonoBehaviour
             instance = this;
         }
         DontDestroyOnLoad(gameObject);
+        unfade = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (fade)
+        {
+            Debug.Log("Fade");
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 1f)
+            {
+                fade = false;
+            }
+        }
+        if (unfade)
+        {
+            Debug.Log("Unfade");
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 0f)
+            {
+                unfade = false;
+            }
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        fade = true;
+        unfade = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        unfade = true;
+        fade = false;
     }
 }
